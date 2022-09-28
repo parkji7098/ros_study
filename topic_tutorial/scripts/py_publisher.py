4a8ea47b80c815e9da4fcc9f0d9e2ef3007f86ee
parkji7098 <wjddlszzzzzzzzzzz11@gmail.com>
2022-09-28 12:10:44 +0900

    add python scripts

---
+++ topic_tutorial/scripts/py_publisher.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher("my_topic", String, queue_size=100)
+    rospy.init_node("py_publisher")
+
+    loop_rate = rospy.Rate(10)
+
+    msg = String()
+    msg.data = "Hi"
+
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        loop_rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        talker()
+    except rospy.ROSInitException:
+        pass
