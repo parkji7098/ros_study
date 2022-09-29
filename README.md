b3de20f902f18531110121d45cdb24e69b9c6685
parkji7098 <wjddlszzzzzzzzzzz11@gmail.com>
2022-09-29 12:49:08 +0900

    update

--- README.md
+++ README.md
@@ -18,7 +18,15 @@ my first ros study
 - 빌드
 - 실행
 
-- 과제
+- [과제 1](./topic_test/)
+
+### 2022년 9월 29일
+- [msg_tutorial](./msg_tutorial)
+    - msg_tutorial 패키지 생성
+    - msg 디렉토리에 Mymsg.msg 생성
+    - msg_publisher, msg_subscriber, py_msg_pub.py, py_msg_sub.py 노드 생성
+    - 빌드
+    - 실행
 
 ## ROS 명령어
 ### roscore
@@ -39,4 +47,9 @@ my first ros study
 '''bash
     catkin_create_pkg <패키지이름> <의존성1> <의존성2> ....
 '''
-
+## msg 만드는 법
+1. 패키지 안에 msg 디렉토리를 만든다.
+2. .msg 확장자의 파일을 만든다.
+3. 안에 내용을 작성한다.
+4. CMakelists.txt에서 설정한다.
+5. 빌드시 생성된다.
---
+++ msg_tutorial/src/msg_publisher.cpp
@@ -0,0 +1,33 @@
+#include "ros/ros.h"
+#include "msg_tutorial/Mymsg.h"
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "msg_publisher");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub = nh.advertise<msg_tutorial::Mymsg>("burger", 20);
+
+    ros::Rate loop_rate(2);
+
+    msg_tutorial::Mymsg msg;
+    int cnt = 0;
+
+    while (ros::ok())
+    {
+        /* msg {
+            time stamp
+            int32 data
+        } */
+        msg.stamp = ros::Time::now(); // 현재 시간을 msg의 stamp에 담는다.
+        msg.data = cnt; // cnt 변수의 값을 msg의 data에 담는다.
+        ROS_INFO("send msg : %d", msg.stamp.sec); // stamp.sec를 출력한다.
+        ROS_INFO("send msg : %d", msg.stamp.nsec); // stamp.nsec를 출력한다.
+        ROS_INFO("send msg : %d", msg.data); // data를 출력한다.
+        pub.publish(msg);
+        cnt++;
+        loop_rate.sleep();
+        
+    }
+    return 0;
+}
